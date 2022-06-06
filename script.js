a3694cd3915e14fe8fc66d9f4f81010d7a4f0b16
Ali <alitine94@gmail.com>
2022-06-06 04:08:05 -0400

    Created variables

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// A good practice when building a project is to select elements we need and store them in variables.
+// A Node list behaves like an array 
+//  Selecting elements we will need and storing them in variables.
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const closeModal = document.querySelector(".close-modal");
+const btnModal = document.querySelectorAll(".show-modal")
+
+for (let i = 0; i < btnModal.length; i++){
+    console.log(btnModal[i].textContent);
+}
