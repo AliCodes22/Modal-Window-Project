59d62aec4ff941c650375d4b00f2d4626e3279e9
Ali <alitine94@gmail.com>
2022-06-09 04:59:16 -0400

    Some comments added . Experimenting with different methods.

--- script.js
+++ script.js
@@ -7,8 +7,19 @@
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const closeModal = document.querySelector(".close-modal");
-const btnModal = document.querySelectorAll(".show-modal")
+const btnModal = document.querySelectorAll(".show-modal");
 
-for (let i = 0; i < btnModal.length; i++){
-    console.log(btnModal[i].textContent);
-}
+// for(let i = 0; i < btnModal.length; i++){
+//     btnModal[i].addEventListener("click",function() {
+//         console.log("The button's clicked")
+//     })
+// }
+
+btnModal.forEach(function(btn){                   // btn is individual element of the array
+    btn.addEventListener("click",function(){
+        document.querySelector("div").classList.remove(".hidden");
+        console.log("It works!");
+    })
+})
+
+// forEach is much cleaner and makes more sense.
