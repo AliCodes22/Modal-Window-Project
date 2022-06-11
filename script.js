26968b266a9489cce6c85840f46d54e1473e921f
Ali <alitine94@gmail.com>
2022-06-11 04:01:24 -0400

    Completed the modal window tutorial. Added notes.

--- script.js
+++ script.js
@@ -3,23 +3,36 @@
 // A good practice when building a project is to select elements we need and store them in variables.
 // A Node list behaves like an array 
 //  Selecting elements we will need and storing them in variables.
+// In the beginning , the modal is hidden. 
+// We select the modal element and remove the hidden class when any of the buttons are clicked
+// We close the modal by re-adding the hidden class whenever the close button is clicked or whenever the overlay is clicked
+// We created two functions that are executed in the event and used the function as the second argument
+// I went for the forEach method as I found it to be more appropriate.
+
 
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
-const closeModal = document.querySelector(".close-modal");
+const btnCloseModal = document.querySelector(".close-modal");
 const btnModal = document.querySelectorAll(".show-modal");
 
-// for(let i = 0; i < btnModal.length; i++){
-//     btnModal[i].addEventListener("click",function() {
-//         console.log("The button's clicked")
-//     })
-// }
+const openModal = function(){
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+}
 
-btnModal.forEach(function(btn){                   // btn is individual element of the array
-    btn.addEventListener("click",function(){
-        document.querySelector("div").classList.remove(".hidden");
-        console.log("It works!");
-    })
+const closeModal = function(){
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+}
+
+btnModal.forEach(function(btn){
+    btn.addEventListener("click",openModal)
 })
 
-// forEach is much cleaner and makes more sense.
+btnCloseModal.addEventListener("click",closeModal);
+overlay.addEventListener("click",closeModal)
+
+
+// Handling Keypress Events
+
+document.addEventListener("keydown",closeModal);
